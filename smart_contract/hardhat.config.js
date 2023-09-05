96a09a9fe0f232e8d5b9a91c8353e98c2219dfe2
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-09-05 14:42:28 +0300

    Setup env variable using dotenv package

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.0",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
