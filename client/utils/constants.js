5f0504b80f51ee76c476543a891982587a696449
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-09-05 14:52:39 +0300

    Get Deplyed address and contract ABI

---
+++ client/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+//This is get form smart_contract/artifacts/Transaction.sol/Transaction.json  Copy that and past to client/src/utiles/Transaction.json
+export const contractABI = abi.abi;
+export const contractAddress = '0x22e9f27a1052bd4E2673c76795b64e7ae9939322'
