ff5c713ee6ce0ab901f0abd3e27e5a675be99b2d
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-09-06 14:18:40 +0300

    Link to my EtherScan to see all transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -13,7 +13,7 @@ const TransactionsCard = ({
   amount,
   url,
 }) => {
-  const gifUrl = useFetch({ keyword });
+  // const gifUrl = useFetch({ keyword });
 
   return (
     <div
@@ -53,11 +53,11 @@ const TransactionsCard = ({
             </>
           )}
         </div>
-        <img
+        {/* <img
           src={gifUrl || url}
           alt="nature"
           className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
-        />
+        /> */}
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
           <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
@@ -83,6 +83,9 @@ const Transactions = () => {
         )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
+          {dummyData.reverse().map((transaction,i)=>(
+            <TransactionsCard key={i} {...transaction}/>
+          ))}
           {/* {[...dummyData, ...transactions].reverse().map((transaction, i) => (
             <TransactionsCard key={i} {...transaction} />
           ))} */}
