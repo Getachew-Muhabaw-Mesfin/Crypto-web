1f13235882c7972915063f7ebc831c9d8853a36b
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-09-07 07:49:04 +0300

    Footer custemization

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 import logo from "../../images/logo.png";
-
+const footerLink = [
+  "Market",
+  "Exchange",
+  "Tutorials",
+  "Wallets",
+  "My Protfolio",
+];
 const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
@@ -9,18 +15,14 @@ const Footer = () => (
         <img src={logo} alt="logo" className="w-32" />
       </div>
       <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Market
-        </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Exchange
-        </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Tutorials
-        </p>
-        <p className="text-white text-base text-center mx-2 cursor-pointer">
-          Wallets
-        </p>
+        {footerLink.map((item, index) => (
+          <p
+            className="text-white text-base text-center mx-2 cursor-pointer"
+            key={index}
+          >
+            {item}
+          </p>
+        ))}
       </div>
     </div>
 
